cbf9b536e4e5d54fd0527be29a48f647177aa63c
Aryan <aryanbrite@gmail.com>
2026-05-25 09:19:00 +0530

    fix: add min_value to height and weight inputs in BMI Calculator

--- pages/BMI Calculator.py
+++ pages/BMI Calculator.py
@@ -12,7 +12,7 @@ with col2:
 
 cl1, cl2 = st.columns(2)
 with cl1:
-    st.number_input("Weight",step=5)
+    st.number_input("Weight",step=5, min_value=0)
 with cl2:
     unitw= st.segmented_control("",["Kg","Pounds"])
 st.write("")
@@ -25,10 +25,16 @@ a1, a2=st.columns(2, vertical_alignment="center")
 if v==False:
     a1, a2=st.columns(2, vertical_alignment="center")
     with a1:
-        st.number_input(label="hight", step=5, label_visibility="collapsed")
+        st.number_input(label="hight", step=5, label_visibility="collapsed", min_value=0)
     with a2:
         st.text("cm")
 else:
     b1, b2, b3, b4= st.columns(4,vertical_alignment="center")
     with b1:
-        st.number_input(lebal="hight", step=5, label_visibility="collapsed")
+        st.number_input(label="hightfee", step=5, label_visibility="collapsed",min_value=0)
+    with b2:
+        st.text("feet")
+    with b3:
+        st.number_input(label="hightin", step=5, label_visibility="collapsed", min_value=0)
+    with b4:
+        st.text("feet")
