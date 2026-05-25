5cb2650a0720ffc4da01824c1e3422456d6dc7ef
Aryan <aryanbrite@gmail.com>
2026-05-25 06:26:33 +0530

    add BMI Calculator page and fix Streamlit page icon formatting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.set_page_config(
     page_title="Calculate",
-    page_icon="💡"
+    page_icon="💡",
 )
 
 st.title("Calculate")
+st.write("Hi, This project is made on Streamlit. A python framework. Expolre to find the things you can find !")
