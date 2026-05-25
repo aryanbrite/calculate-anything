0e24548f6c924e8665d01a93bb608126d4a77ca9
Aryan <aryanbrite@gmail.com>
2026-05-25 07:58:14 +0530

    refactor: restructure BMI Calculator layout with columns and image

--- pages/BMI Calculator.py
+++ pages/BMI Calculator.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.title("BMI Calculator")
+st.set_page_config(layout="wide")
+col1, col2, col3, col4=st.columns(4, gap="xxsmall")
+with col1:
+    st.image("https://static.vecteezy.com/system/resources/thumbnails/068/360/341/small/cute-cartoon-brown-cat-peeking-over-line-illustration-kawaii-cat-sticker-hand-drawn-cartoon-free-png.png",width=200)
+with col2:    
+    st.title("BMI Calculator")
