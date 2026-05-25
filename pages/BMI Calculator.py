3b39a74e2cbb5d19701c6e6da147681ec8d72e84
Aryan <aryanbrite@gmail.com>
2026-05-25 08:14:36 +0530

    fix: add current date display to BMI Calculator

--- pages/BMI Calculator.py
+++ pages/BMI Calculator.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from datetime import date
+at=date.today()
+dat=str(at)
 col1, col2=st.columns([3,10], gap="xsmall", vertical_alignment="center")
 with col1:
     st.image("https://static.vecteezy.com/system/resources/thumbnails/022/533/274/small/hand-drawn-cute-white-bear-feel-happy-in-doodle-style-png.png",width=200)
 with col2:    
     st.title("BMI Calculator")
+
+st.write(dat)
