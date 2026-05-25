e3db2fc49d898830491219f629887e8a3384ed4e
Aryan <aryanbrite@gmail.com>
2026-05-25 08:06:45 +0530

    refactor: update layout and image in BMI Calculator

--- pages/BMI Calculator.py
+++ pages/BMI Calculator.py
@@ -1,7 +1,6 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-col1, col2, col3, col4=st.columns(4, gap="xxsmall")
+col1, col2=st.columns([3,10], gap="xsmall", vertical_alignment="center")
 with col1:
-    st.image("https://static.vecteezy.com/system/resources/thumbnails/068/360/341/small/cute-cartoon-brown-cat-peeking-over-line-illustration-kawaii-cat-sticker-hand-drawn-cartoon-free-png.png",width=200)
+    st.image("https://static.vecteezy.com/system/resources/thumbnails/022/533/274/small/hand-drawn-cute-white-bear-feel-happy-in-doodle-style-png.png",width=200)
 with col2:    
     st.title("BMI Calculator")
