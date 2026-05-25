e9cb2c6283fe682d8ecefbdb9b2e694aa9610ca5
Aryan <aryanbrite@gmail.com>
2026-05-25 08:23:52 +0530

    fix: update image in BMI Calculator to a new asset

--- pages/BMI Calculator.py
+++ pages/BMI Calculator.py
@@ -1,8 +1,11 @@
 import streamlit as st
 col1, col2=st.columns([3,10], gap="xsmall", vertical_alignment="center")
 with col1:
-    st.image("https://static.vecteezy.com/system/resources/thumbnails/022/533/274/small/hand-drawn-cute-white-bear-feel-happy-in-doodle-style-png.png",width=200)
+    st.image("images/favpng_7d1161465538a1c0897ab46880a7ca8b.png",width=150)
 with col2:    
     st.title("BMI Calculator")
+
+
+
 
 
